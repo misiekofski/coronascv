13d9f89a91a456544e0d2b021ba0c6c0170f6bc7
Michal Dobrzycki <dobrzycki@gmail.com>
2021-01-12 21:44:52 +0100

    Added support for CSV files

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def corona_plot():
-    return render_template('index.html')
+    dataset = pd.read_csv('data.csv')
+    labels = [str(x) for x in dataset['date']]
+    values = [x for x in dataset['vaccine_number']]
+    return render_template('index.html', labels=labels, values=values)
 
 
 if __name__ == '__main__':
