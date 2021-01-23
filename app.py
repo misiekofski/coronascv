b5d46c0be8e56fbc7f0ad3f77b0a2c0a28d34828
Michal Dobrzycki <dobrzycki@gmail.com>
2021-01-23 16:35:26 +0100

    Added options to cut scope to latest 7 and 14 days

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ from flask import request
 from datetime import date
 import pandas as pd
 
-start_date = date(2020,12,27)
+start_date = date(2020, 12, 27)
 today = date.today()
 app = Flask(__name__)
 
 
 @app.route('/')
 def corona_plot():
+    days_from_start = (today - start_date).days
     days = int(request.args.get('ld', default=0))
-    days_from_start = (today-start_date).days
+    add_days = days_from_start - days
     dataset = pd.read_csv('data.csv')
     labels = [x for x in dataset['date']][-days:]
     values = [x for x in dataset['vaccine_number']][-days:]
-    return render_template('index.html', labels=labels, values=values, days_from_start=days_from_start)
+    return render_template('index.html', labels=labels, values=values, days_from_start=days_from_start,
+                           add_days=add_days)
 
 
 if __name__ == '__main__':
