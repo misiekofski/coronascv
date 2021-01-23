c58e2a2869e438884c74c51d96461fb56ad3ea17
Michal Dobrzycki <dobrzycki@gmail.com>
2021-01-23 14:32:59 +0100

    Added options for 14 and 28 days

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
+from flask import request
+from datetime import date
 import pandas as pd
 
-
+start_date = date(2020,12,27)
+today = date.today()
 app = Flask(__name__)
 
 
 @app.route('/')
 def corona_plot():
+    days = int(request.args.get('ld', default=0))
+    days_from_start = (today-start_date).days
     dataset = pd.read_csv('data.csv')
-    labels = [x for x in dataset['date']]
-    values = [x for x in dataset['vaccine_number']]
-    return render_template('index.html', labels=labels, values=values)
+    labels = [x for x in dataset['date']][-days:]
+    values = [x for x in dataset['vaccine_number']][-days:]
+    return render_template('index.html', labels=labels, values=values, days_from_start=days_from_start)
 
 
 if __name__ == '__main__':
